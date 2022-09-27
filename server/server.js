2c4c9f8167cd89b84ec29c5db64cdc7e9a99e1e3
Daniel Legan <legan.daniel@gmail.com>
2022-09-27 16:41:40 -0500

    more server stuff

--- server/server.js
+++ server/server.js
@@ -2,9 +2,33 @@ const express = require('express');
 
 const app = express();
 
+let comments = [
+    'servers are neat',
+    'well, they are not that cool',
+    'cool story bro'
+];
+
 // make files in the server/public folder
 // available to anyone who wants them
 app.use(express.static('./server/public'));
+
+// app.on('get');
+// like an "event handler"
+// when Jimmy asks for comments
+// http://localhost:3000/comments
+// call this function
+
+app.get('/comments', (req, res) => {
+    console.log('Jimmy wants those comments!');
+
+    res.send(comments);
+});
+
+app.get('/comments/first', (req, res) => {
+    console.log('in /comments/first');
+
+    res.send(comments[0]);
+});
 
 app.listen(3000, () => {
     console.log('we are live!');
